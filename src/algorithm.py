9709229b674f4de630a6a8402b77d6af5bc5d5f8
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-04-06 23:14:04 +0700

    fix: algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -5,34 +5,37 @@ class UCS :
     def ucs(start, goal, graph):
         # Initialize the frontier queue with the start node and its priority
         frontier = [(0, start)]
+
         # Initialize the explored set to an empty set
         explored = set()
+
         # Initialize the cost of the start node to 0
         node_cost = {start: 0}
+
         # Initialize the parent of the start node to None
         parent = {start: None}
 
         # Keep searching until the frontier queue is empty
         while frontier:
             # Get the node with the lowest cost from the frontier queue
-            cost, current = heappop(frontier)
+            currentCost, currentNode = heappop(frontier)
 
-            # Check if the current node is the goal node
-            if current == goal:
+            # Check if the currentNode node is the goal node
+            if currentNode == goal:
                 path = []
                 # Reconstruct the path from the start node to the goal node using the parent dictionary
-                while current:
-                    path.append(current)
-                    current = parent[current]
+                while currentNode:
+                    path.append(currentNode)
+                    currentNode = parent[currentNode]
                 return list(reversed(path)), node_cost[goal]
 
-            # Add the current node to the explored set
-            explored.add(current)
+            # Add the currentNode node to the explored set
+            explored.add(currentNode)
 
-            # Explore the neighbors of the current node
-            for neighbor, neighbor_cost in graph[current].items():
+            # Explore the neighbors of the currentNode node
+            for neighbor, neighbor_cost in graph[currentNode].items():
                 # Calculate the total cost of reaching the neighbor from the start node
-                total_cost = node_cost[current] + neighbor_cost
+                total_cost = node_cost[currentNode] + neighbor_cost
 
                 # Check if the neighbor is already explored or in the frontier queue with a lower cost
                 if neighbor in explored:
@@ -43,7 +46,7 @@ class UCS :
                 # If the neighbor is not already explored or in the frontier queue with a lower cost,
                 # update its cost and parent in the node_cost and parent dictionaries, and add it to the frontier queue
                 node_cost[neighbor] = total_cost
-                parent[neighbor] = current
+                parent[neighbor] = currentNode
                 heappush(frontier, (total_cost, neighbor))
 
         # If the goal node is not found and the frontier queue is empty, return None
@@ -58,37 +61,39 @@ class aStar :
     def astar(start, goal, graph, heuristic):
         # Initialize start node and open set
         open_set = [(0, start)]
+
         # Keep track of parent nodes for each visited node
         parents = {start: None}
+
         # Cost of getting to start node is zero
-        g_scores = {start: 0}
+        gn = {start: 0}
 
         while open_set:
             # Get the node with the lowest f-score
-            current_g, current = heappop(open_set)
+            currentCost, currentNode = heappop(open_set)
 
-            # If current is goal, return the path
-            if current == goal:
+            # If currentNode is goal, return the path
+            if currentNode == goal:
                 path = []
-                while current:
-                    path.append(current)
-                    current = parents[current]
-                return path[::-1]
+                while currentNode:
+                    path.append(currentNode)
+                    currentNode = parents[currentNode]
+                return path[::-1], gn
 
-            # Check neighbors of current node
-            for neighbor, cost in graph[current]:
+            # Check neighbors of currentNode node
+            for neighbor, cost in graph[currentNode]:
                 # Calculate g-score of neighbor
-                g_score = g_scores[current] + cost
+                gn = gn[currentNode] + cost
 
                 # Calculate f-score of neighbor
-                h_score = heuristic(neighbor, goal)
-                f_score = g_score + h_score
+                hn = heuristic(neighbor, goal)
+                fn = gn + hn
 
                 # If neighbor is not in open set or has lower f-score, update values
-                if neighbor not in g_scores or g_score < g_scores[neighbor]:
-                    g_scores[neighbor] = g_score
-                    parents[neighbor] = current
-                    heappush(open_set, (f_score, neighbor))
+                if neighbor not in gn or gn < gn[neighbor]:
+                    gn[neighbor] = gn
+                    parents[neighbor] = currentNode
+                    heappush(open_set, (fn, neighbor))
 
         # If goal is not reachable, return empty path
         return []
