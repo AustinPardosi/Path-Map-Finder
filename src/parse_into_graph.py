a979825c3e97b88497383183398743d0528787a5
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-04-07 20:36:15 +0700

    fix: parser (done)

--- src/parse_into_graph.py
+++ src/parse_into_graph.py
@@ -1,3 +1,39 @@
+import math
+def distance(p1, p2):
+    lat1, lon1 = p1
+    lat2, lon2 = p2
+    return math.sqrt((lat1-lat2)**2 + (lon1-lon2)**2)
+
+def parse_into_adjacency_mtr(filename):
+    # Parse nodes and coordinates
+    nodes = {}
+    with open(filename) as f:
+        lines = f.readlines()
+        n = int(lines[0].strip())
+        listnodes = []
+        for line in lines[1:n+1]:
+            label, lat, lon = line.strip().split()
+            nodes[label] = (float(lat), float(lon))
+            listnodes.append(label)
+
+
+        m = n + 1
+        print(m)
+
+        matrix = lines[9:]
+        mtr = [[int(x) for x in line.split()] for line in matrix]
+
+        for i in range(n) :
+            for j in range(n) :
+                if mtr[i][j] == 1 :
+                    p1 = nodes[listnodes[j]]
+                    p2 = nodes[listnodes[i]]
+                    mtr[i][j] = distance(p1, p2)
+
+        # print(mtr)
+
+    return mtr
+
 def parse_adjacency_matrix(adj_matrix):
     graph = {}
     countNodes = len(adj_matrix)
@@ -12,6 +48,21 @@ def parse_adjacency_matrix(adj_matrix):
         graph[node] = edges
 
     return graph
+
+def print_graph(graph):
+    for node, edges in graph.items():
+        print(f"{node} -> ", end="")
+        if not edges:
+            print("None")
+        else:
+            for neighbor, weight in edges.items():
+                print(f"{neighbor} ({weight}), ", end="")
+            print()
+
+mtr = parse_into_adjacency_mtr('src/tes.txt')
+graph = parse_adjacency_matrix(mtr)
+
+print_graph(graph)
 
 # adj_matrix = [
 #     [0, 2, 5, 0, 0, 0, 0, 0],
