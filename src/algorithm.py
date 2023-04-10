3bf1e70187a4c020c1ca34a9849e26b12d448fa1
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-04-10 22:09:06 +0700

    fix: algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -4,112 +4,81 @@ import parse_into_graph as parser
 
 class UCS :
     def ucs(start, goal, graph):
-        # Initialize the frontier queue with the start node and its priority
-        frontier = [(0, start)]
-
-        # Initialize the explored set to an empty set
-        explored = set()
-
-        # Initialize the cost of the start node to 0
-        node_cost = {start: 0}
-
-        # Initialize the parent of the start node to None
-        parent = {start: None}
-
-        # Keep searching until the frontier queue is empty
-        while frontier:
-            # Get the node with the lowest cost from the frontier queue
-            currentCost, currentNode = heappop(frontier)
-
-            # Check if the currentNode node is the goal node
-            if currentNode == goal:
-                path = []
-                # Reconstruct the path from the start node to the goal node using the parent dictionary
-                while currentNode:
-                    path.append(currentNode)
-                    currentNode = parent[currentNode]
-                return list(reversed(path)), node_cost[goal]
-
-            # Add the currentNode node to the explored set
-            explored.add(currentNode)
-
-            # Explore the neighbors of the currentNode node
-            for neighbor in graph.neighbors(currentNode):
-                # Calculate the total cost of reaching the neighbor from the start node
-                total_cost = node_cost[currentNode] + graph[currentNode][neighbor]['weight']
-
-                # Check if the neighbor is already explored or in the frontier queue with a lower cost
-                if neighbor in explored:
-                    continue
-                if (total_cost, neighbor) in frontier:
-                    continue
-
-                # If the neighbor is not already explored or in the frontier queue with a lower cost,
-                # update its cost and parent in the node_cost and parent dictionaries, and add it to the frontier queue
-                node_cost[neighbor] = total_cost
-                parent[neighbor] = currentNode
-                heappush(frontier, (total_cost, neighbor))
-
-        # If the goal node is not found and the frontier queue is empty, return None
-        return None
-
-class aStar :
-    def euclidean_distance(a, b):
-        x1, y1 = a
-        x2, y2 = b
-        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-    def astar(start, goal, graph, nodes, heuristic):
-        # Initialize start node and open set
-        open_set = [(0, start)]
-
-        # Keep track of parent nodes for each visited node
+        nodesToExplore = [(0, start)]
+        visited = []
+        cost = {start: 0}
         parents = {start: None}
 
-        # Cost of getting to start node is zero
-        gn = {start: 0}
-
-        while open_set:
-            # Get the node with the lowest f-score
-            currentCost, currentNode = heappop(open_set)
-
-            # If currentNode is goal, return the path
+        while nodesToExplore:
+            currentCost, currentNode = heappop(nodesToExplore)
             if currentNode == goal:
                 path = []
                 while currentNode:
                     path.append(currentNode)
                     currentNode = parents[currentNode]
-                return path[::-1], jarak
+                return path[::-1], cost[goal]
 
-            # Check neighbors of currentNode node
+            visited.append(currentNode)
+
             for neighbor in graph.neighbors(currentNode):
-                # Calculate g-score of neighbor
-                jarak = gn[currentNode] + graph[currentNode][neighbor]['weight']
+                total_cost = cost[currentNode] + graph[currentNode][neighbor]['weight']
 
-                # Calculate f-score of neighbor
-                if heuristic :
-                    h_score = aStar.euclidean_distance(nodes[neighbor], nodes[goal])
-                f_score = jarak + h_score
+                if neighbor in visited:
+                    continue
+                if (total_cost, neighbor) in nodesToExplore:
+                    continue
 
-                # If neighbor is not in open set or has lower f-score, update values
-                if neighbor not in gn or jarak < gn[neighbor]:
-                    gn[neighbor] = jarak
+                cost[neighbor] = total_cost
+                parents[neighbor] = currentNode
+                heappush(nodesToExplore, (total_cost, neighbor))
+
+        return [], 0
+
+class aStar :
+    def find_euclidean_distance(a, b):
+        x1, y1 = a
+        x2, y2 = b
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    def astar(start, goal, graph, nodes, heuristic_func):
+        exploreNodes = [(0, start)]
+        parents = {start: None}
+        currentPathWithCost = {start: 0}
+
+        while exploreNodes:
+            currentCost, currentNode = heappop(exploreNodes)
+
+            if currentNode == goal:
+                shortestPath = []
+                while currentNode:
+                    shortestPath.append(currentNode)
+                    currentNode = parents[currentNode]
+                return shortestPath[::-1], fn
+
+            for neighbor in graph.neighbors(currentNode):
+                gn = currentPathWithCost[currentNode] + graph[currentNode][neighbor]['weight']
+
+                if heuristic_func:
+                    hn = aStar.find_euclidean_distance(nodes[neighbor], nodes[goal])
+                    fn = gn + hn
+
+                if (neighbor not in currentPathWithCost) or (gn < currentPathWithCost[neighbor]):
+                    currentPathWithCost[neighbor] = gn
                     parents[neighbor] = currentNode
-                    heappush(open_set, (f_score, neighbor))
+                    heappush(exploreNodes, (fn, neighbor))
 
-        # If goal is not reachable, return empty path
         return [], 0
 
 ##### MAIN #####
 
-# mtr, nodes, listnodes = parser.parse_into_adjacency_mtr('D:\Vs Code\Tucil3_13521063_13521084\src\\tes.txt')
+# mtr, nodes, listnodes = parser.parse_into_adjacency_mtr('src/tes.txt')
 # graph = parser.parse_adjacency_matrix(mtr)
 
 # # A*
-# # start = 'A'
-# # goal = 'F'
-# # heuristic = True
-# # path = aStar.astar(start, goal, graph, nodes, heuristic)
+# start = 'A'
+# goal = 'F'
+# heuristic = True
+# path = aStar.astar(start, goal, graph, nodes, heuristic)
 
 # # UCS
 # start = 'A'
